0259d9142cc1eb9fa45708a7bb0d769322896070
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-03-29 20:45:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(nome);
 
 const primeiroNumero = 5;
 const segundoNumero = 3;
-
 const conta = primeiroNumero * segundoNumero;
-const resultadoDuplicado = conta + 10;
-console.log(resultadoDuplicado);
+const resultadoDuplicado = conta * 10;
+let resultadoTriplicado = resultadoDuplicado * 3
+console.log(resultadoTriplicado);
